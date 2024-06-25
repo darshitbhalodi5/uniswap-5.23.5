cdc876dc8103472d546e5b5b604dbf4b7ba13c5f
darshitbhalodilampros <darshitbhalodia@lampros.tech>
2024-06-25 09:25:24 +0530

    new list added for mode

--- apps/web/src/constants/lists.ts
+++ apps/web/src/constants/lists.ts
@@ -30,7 +30,7 @@ const SET_LIST =
   "https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json";
 const WRAPPED_LIST = "wrapped.tokensoft.eth";
 
-export const MODE_LIST = "https://api.udonswap.org/tokens";
+export const MODE_LIST = "https://tokenlist-api.vercel.app/v3-tokens";
 export const OPTIMISM_LIST =
   "https://static.optimism.io/optimism.tokenlist.json";
 export const ARBITRUM_LIST = "https://bridge.arbitrum.io/token-list-42161.json";
